4e8666e5fdb24bd22ad03d6df9eec288b14c3c23
J47D\Jemel <jemelpastor@gmail.com>
2017-06-10 23:23:18 -0400

    cambios realizados

---
+++ application/libraries/FrontLib.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jemel
+ * Date: 24/05/2017
+ * Time: 21:43
+ */ 
---
+++ application/libraries/SolicitudLib.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jemel
+ * Date: 13/05/2017
+ * Time: 15:44
+ */
+
+class SolicitudLib {
+
+} 
